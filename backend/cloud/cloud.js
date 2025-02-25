dd8516cd98c8b168e413b5a436de7a95b50f55f6
Yuri da Silva Ferreira <142122885+Yuridevpro@users.noreply.github.com>
2025-02-25 19:28:15 +0000

    atualizações 03

--- backend/cloud/cloud.js
+++ backend/cloud/cloud.js
@@ -1,4 +1,14 @@
-// backend/cloud.js
+ // backend/cloud.js
+
+// *** Middleware CORS ***
+Parse.Cloud.before("*", (req, res, next) => {
+  res.set({
+    "Access-Control-Allow-Origin": "git-lab-replit.vercel.app", // Substitua pelo domínio do seu Vercel
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, X-Parse-Application-Id, X-Parse-REST-API-Key, X-Parse-Session-Token",
+  });
+  next();
+});
 
 // *** Funções de Autenticação ***
 require("./autenticacao");
